8815a928b6bfd2d9ce918b9bf8379acd79641bbd
Khalil <khalil.zayed@uoit.net>
2019-05-20 04:43:52 -0400

    second commit

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,13 @@ import rootReducer from './reducers';
 
 const middleware = [thunk];
 
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//     }) : compose;
+
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
